6149554620a3935379aff5ceab0b0aa790e491c3
Francesco Pagano <66972159+francescop21@users.noreply.github.com>
2021-02-03 20:06:22 -0300

    Contador
    contador

--- src/Contenedor/ItemListContainer.jsx
+++ src/Contenedor/ItemListContainer.jsx
@@ -1,25 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Counter from '../Componentes/Contador/ItemCount.jsx';
 
 const Productos = () => {
-    return (
-        <>
-        
-        <div class="container">
-  <div class="row">
-    <div class="col-sm">
-        Producto 1
-    </div>
-    <div class="col-sm">
-        Producto 2
-    </div>
-    <div class="col-sm">
-        Producto 3
-    </div>
-  </div>
+    const ofertas = () => {
+        alert("Ofertas");
+    }
+return (
+    <>   
+    <h1>Nuestros productos</h1>
+    <button OnClick={ofertas}>Ver Ofertas</button>  
+    <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    Producto 1
+                </div>
+                <div class="col-sm">
+                    Producto 2
+                </div>
+                <div class="col-sm">
+                    Producto 3
+                </div>
+        </div>
 </div>
-
-        </>
-    );
+    <Counter/> 
+    </>   
+);
 }
 
 export default Productos; 
